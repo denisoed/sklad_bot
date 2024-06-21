3d9436fdb9f722faa958f9ef5918b2f12fe0f927
Denisoed <denisoed@gmal.com>
2024-06-21 21:43:37 +0600

    Init

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { Telegraf, Markup } from 'telegraf';
+
+const bot = new Telegraf(process.env.BOT_TOKEN as string);
+const URL = process.env.BOT_URL as string;
+
+const welcomeMessage = `
+Добро пожаловать на Sklad!
+
+Здесь вы можете легко и эффективно управлять своими товарами.
+`;
+
+bot.start((ctx) => {
+  ctx.reply(
+		welcomeMessage,
+		Markup.inlineKeyboard([Markup.button.webApp('Открыть склад', URL)]),
+	)
+});
+
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
